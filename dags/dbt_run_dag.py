8646dd015d60dec3be3a7e0d56f661f0faa02898
Andrew Bakos <andrew@romutrechtregion.nl>
2025-03-11 13:56:04 +0000

    update andrew

--- dags/dbt_run_dag.py
+++ dags/dbt_run_dag.py
@@ -2,23 +2,31 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
+# Default arguments for tasks
 default_args = {
     "owner": "airflow",
     "start_date": datetime(2025, 3, 1),
     "retries": 1,
+    "depends_on_past": False,
 }
 
+# Define the DAG
 dag = DAG(
-    "dbt_run",
+    dag_id="dbt_run",
     default_args=default_args,
+    description="Run dbt models using BashOperator",
     schedule_interval="@daily",
     catchup=False,
 )
 
+# Task to run dbt
 dbt_run = BashOperator(
     task_id="dbt_run",
     bash_command="DBT_PROFILES_DIR=/opt/dbt dbt run --project-dir /opt/dbt",
     dag=dag,
+    env={"DBT_PROFILES_DIR": "/opt/dbt"}
 )
 
+
+# Define DAG structure
 dbt_run
